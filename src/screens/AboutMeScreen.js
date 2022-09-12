d53baac721e4c5c53b2d3c2cfed0938e820f68a2
WelBert-dev <wellison.bertelli@hotmail.com>
2022-09-12 14:22:38 -0300

    melhora designer do terminal em aboutMe

--- src/screens/AboutMeScreen.js
+++ src/screens/AboutMeScreen.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { TbCodePlus, TbArrowsMaximize, TbArrowsDiagonalMinimize2 } from 'react-icons/tb';
+import { BiSearchAlt } from 'react-icons/bi';
+import { GiHamburger } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import './AboutMeScreen.css';
 
@@ -9,17 +13,33 @@ export default function AboutMeScreen() {
             <div>
                 <h1 className="main-title">&lsaquo;Sobre Mim&rsaquo;</h1>
                 <div>
-                    <p>
-                    $ Desenvolvedor e entusiasta do mundo da programação e tecnologia, focado na web2.0 de ponta a ponta 
-                    com boa base na stack Backend e atualmente focado nas tecnologias Frontend, buscando experiência
-                    em projetos reais e/ou em produção, vivendo e estudando programação todos os dias de segunda a segunda a 2 anos,
-                    pois alguém uma vez me disse "Obsessão vence do talento natural", e esta frase vem fazendo mais sentido cada dia!
-                    Sonhador e de uma familia humilde, um dia com certeza irei proporcionar melhoria aos meus..
+                    <div>
+                        <div className="main-sobre--shellHead">
+                            <i><TbCodePlus /></i>
+                            <span><h3>root@r2d2:/home/welbert/aboutMe</h3></span>
+                            <div>
+                                <i><BiSearchAlt /></i>
+                                <i><GiHamburger /></i>
+                            </div>
+                            <div className="main-sobre--shellHeadWindowCommomIcons">
+                                <i><TbArrowsDiagonalMinimize2 /></i>
+                                <i><TbArrowsMaximize /></i>
+                                <i><AiOutlineClose /></i>
+                            </div>
 
-                    "Um leão por dia", acredito que um dia chego la!
+                        </div>
+                        <p>
+                        <span class="main-sobre--shellUser">root@r2d2</span>:<span className="main-sobre--shellDir">/home/welbert/aboutMe</span># \ <br/>> Desenvolvedor e entusiasta do mundo da programação e tecnologia, focado na web2.0 de ponta a ponta 
+                        com boa base na stack Backend e atualmente focado nas tecnologias Frontend, buscando experiência
+                        em projetos reais e/ou em produção, vivendo e estudando programação todos os dias de segunda a segunda a 2 anos,
+                        pois alguém uma vez me disse "Obsessão vence do talento natural", e esta frase vem fazendo mais sentido cada dia!
+                        Sonhador e de uma familia humilde, um dia com certeza irei proporcionar melhoria aos meus..
 
-                    Obs: Trabalhando como freelancer nos dias atuais. $
-                    </p>
+                        "Um leão por dia", acredito que um dia chego la!
+
+                        Obs: Trabalhando como freelancer nos dias atuais.
+                        </p>
+                    </div>
                     <ul>
                         <li>Nacionalidade: Brasileira</li>
                         <li>Idade: 22 Anos</li>
