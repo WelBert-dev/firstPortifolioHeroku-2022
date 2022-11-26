7b0bf2a0e7d44e04aeea9f23165182f986805514
WelBert-dev <wellison.bertelli@hotmail.com>
2022-11-26 13:06:56 -0300

    add news skills e data de instrução para obsoletos

--- src/screens/SkillsScreen.js
+++ src/screens/SkillsScreen.js
@@ -19,14 +19,14 @@ export default function SkillsScreen() {
                         <img className="icon" src="https://cdn.iconscout.com/icon/free/png-256/javascript-2038874-1720087.png" alt="Logo do JavaScript" ></img>
                         <div className="right">
                             <h3 className="title-skill">JavaScript</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
+                            <progress className="progress-bar" max="100" value="80">80%</progress>
                         </div>
                     </article>
                     <article className="skill-tec">
                         <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png" alt="Logo do HTML5" ></img>
                         <div className="right">
                             <h3 className="title-skill">HTML5</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
+                            <progress className="progress-bar" max="100" value="90">90%</progress>
                         </div>
                     </article>
                     <article className="skill-tec">
@@ -40,21 +40,7 @@ export default function SkillsScreen() {
                         <img className="icon" src="https://gitlab.com/uploads/-/system/project/avatar/14519101/react-256.png" alt="Logo do ReacJs" ></img>
                         <div className="right">
                             <h3 className="title-skill">ReactJs</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
-                        </div>
-                    </article>
-                    <article className="skill-tec">
-                        <img className="icon" src="https://cdn.cdnlogo.com/logos/c/27/c.svg" alt="Logo do C#" ></img>
-                        <div className="right">
-                            <h3 className="title-skill">C#</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
-                        </div>
-                    </article>
-                    <article className="skill-tec">
-                        <img className="icon" src="https://miro.medium.com/max/300/1*oPEDLkxFxRbrzSBvDSt72g.png" alt="Logo do AspNetCore" ></img>
-                        <div className="right">
-                            <h3 className="title-skill">Asp.NetCore</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
+                            <progress className="progress-bar" max="100" value="75">75%</progress>
                         </div>
                     </article>
                     <article className="skill-tec">
@@ -65,17 +51,49 @@ export default function SkillsScreen() {
                         </div>
                     </article>
                     <article className="skill-tec">
-                        <img className="icon" src="https://cdn.iconscout.com/icon/free/png-256/python-3521655-2945099.png" alt="Logo do Python" ></img>
-                        <div className="right">
-                            <h3 className="title-skill">Python</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
-                        </div>
-                    </article>
-                    <article className="skill-tec">
                         <img className="icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Gnulinux.svg/1200px-Gnulinux.svg.png" alt="Logo do GNU/Linux" ></img>
                         <div className="right">
                             <h3 className="title-skill">GNU/Linux</h3>
-                            <progress className="progress-bar" max="100" value="70">70%</progress>
+                            <progress className="progress-bar" max="100" value="90">90%</progress>
+                        </div>
+                    </article>
+                    <article className="skill-tec">
+                        <img className="icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTim1Q8F1L5j-MqUTl_J_dHHHHeSFmTSz102sZye0U3iNEUdvLMPevqIYCkLzrNk2KioG4&usqp=CAU" alt="Logo do Git" ></img>
+                        <div className="right">
+                            <h3 className="title-skill">Git</h3>
+                            <progress className="progress-bar" max="100" value="65">65%</progress>
+                        </div>
+                    </article>
+                    <article className="skill-tec">
+                        <img className="icon" src="https://camo.githubusercontent.com/c5958aac2c6cd8b680061594f9924e2db27abf2acc4717c207c2687bc693de65/68747470733a2f2f63646e2d696d616765732d312e6d656469756d2e636f6d2f6d61782f3235362f312a46454539386957696e6c5a42596b78424147384d76412e706e67" alt="Logo do shell bash" ></img>
+                        <div className="right">
+                            <h3 className="title-skill">ShellScript Bash</h3>
+                            <progress className="progress-bar" max="100" value="30">30%</progress>
+                            <span>08/2021 até 10/2021</span>
+                        </div>
+                    </article>
+                    <article className="skill-tec">
+                        <img className="icon" src="https://cdn.cdnlogo.com/logos/c/27/c.svg" alt="Logo do C#" ></img>
+                        <div className="right">
+                            <h3 className="title-skill">C#</h3>
+                            <progress className="progress-bar" max="100" value="50">50%</progress>
+                            <span>06/2021 até 12/2021</span>
+                        </div>
+                    </article>
+                    <article className="skill-tec">
+                        <img className="icon" src="https://miro.medium.com/max/300/1*oPEDLkxFxRbrzSBvDSt72g.png" alt="Logo do AspNetCore" ></img>
+                        <div className="right">
+                            <h3 className="title-skill">Asp.NetCore</h3>
+                            <progress className="progress-bar" max="100" value="30">30%</progress>
+                            <span>08/2021 até 12/2021</span>
+                        </div>
+                    </article>
+                    <article className="skill-tec">
+                        <img className="icon" src="https://cdn.iconscout.com/icon/free/png-256/python-3521655-2945099.png" alt="Logo do Python" ></img>
+                        <div className="right">
+                            <h3 className="title-skill">Python</h3>
+                            <progress className="progress-bar" max="100" value="50">50%</progress>
+                            <span>01/2021 até 06/2021</span>
                         </div>
                     </article>
                 </div>
